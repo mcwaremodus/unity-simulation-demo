866e62d3d1b7eccd598cd603faa95ea0e81f9f94
mcwaremodus <=>
2020-02-10 09:48:15 +0000

    0.1_Initial commit, pipeline and realistic environment map, Sabretooth agent with navigation and basic survey spotlight

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    SabreHandler sHand;
+
+    Vector3 moveInput;
+    Vector3 rotInput;
+
+    bool powered = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sHand = GetComponent<SabreHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Receive input
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        float u = Input.GetAxisRaw("Custom");
+
+        float rh = Input.GetAxisRaw("Mouse X");
+        float rv = Input.GetAxisRaw("Mouse Y");
+        float ru = Input.GetAxisRaw("Steve");
+
+        moveInput = new Vector3(h, u, v);
+        rotInput = new Vector3(rv, rh, ru);
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            powered = !powered;
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        // Send input
+        sHand.MoveInput(moveInput, rotInput, powered);
+    }
+}
---
+++ Assets/Scripts/SabreHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SabreHandler : MonoBehaviour
+{
+
+    Vector3 posInput;
+    Vector3 rotInput;
+
+    bool powered = true;
+    float speed = 150;
+
+    public void MoveInput (Vector3 move, Vector3 rote, bool power)
+    {
+        posInput = move;
+        rotInput = rote;
+        powered = power;
+
+        ActuallyMove();
+    }
+
+    void ActuallyMove()
+    {
+        if (powered)
+        {
+            speed = 150;
+        }
+        else
+        {
+            speed = 0;
+            GetComponent<Rigidbody>().drag = 0;
+        }
+
+        GetComponent<Rigidbody>().AddRelativeForce(posInput * speed);
+        GetComponent<Rigidbody>().AddRelativeTorque(rotInput);
+    }
+}
