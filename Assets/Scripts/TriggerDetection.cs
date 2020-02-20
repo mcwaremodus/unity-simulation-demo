93ceb272f4f59468bf5eecbf7dda0d101ce98345
mcwaremodus <=>
2020-02-20 15:57:08 +0000

    0.2_Added imported real HAUV-2 model and modified in Blender, started work on collision detection for side scanners

---
+++ Assets/Scripts/TriggerDetection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetection : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        print("Collision detected with trigger object " + other.name);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        print("Still colliding with trigger object " + other.name);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
+    }
+}
