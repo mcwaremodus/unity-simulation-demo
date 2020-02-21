9302b4a99f2e4b88c43767a7bf3d12c5ae13f632
mcwaremodus <=>
2020-02-21 13:18:46 +0000

    0.21_Fixed no collision between side-light cone and terrain by enabled 'kinesmatic static pairs'

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -8,15 +8,47 @@ public class CollisionDetection : MonoBehaviour
         print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
         print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
         print("Their relative velocity is " + collisionInfo.relativeVelocity);
+
+
+        if (collisionInfo.collider.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        {
+            print("ENVIRONMENT HIT");
+        }
+        
+
+    
+
     }
 
     void OnCollisionStay(Collision collisionInfo)
     {
-        print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+        //print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+
+        // var contact = collisionInfo.contacts[0];
+        //print("Contact point is " + contact.point); //this is the Vector3 position of the point of contact
+
+        foreach (ContactPoint contact in collisionInfo.contacts) {
+            if (Random.value > 0.0)
+                DrawHit(contact); 
+        }
     }
 
     void OnCollisionExit(Collision collisionInfo)
     {
         print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+    }
+
+    void DrawHit(ContactPoint contact)
+    {
+        // Visualize the contact point
+        Debug.DrawRay(contact.point, contact.normal, Color.red);
+
+        //var mySphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //mySphere.transform.localScale = new Vector3(4, 4, 4);
+        //mySphere.GetComponent<Renderer>().material.color = new Color(250, 0, 0, 1);
+        //mySphere.transform.position = pos;
+
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawSphere(pos, 1);
     }
 }
